660311f5abd08585451c36395d76eff97fa7590a
Sebalaur <75026257+Sebalaur@users.noreply.github.com>
2020-11-26 19:51:38 +0200

    iii
    nhdhd

---
+++ First 2D/Assets/NewBehaviourScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+public GameObject collectable;
+public GameObject badCollectable;
+public float collectableSpawnTime = 1.0f;
+public float badCollectableSpawnTime = 2.0f;
+float maxX = 17.5f;
+float maxY = 9.0f;
+float randomXCollectable = 0.0f;
+float randomYCollectable = 0.0f;
+float randomXBadCollectable = 0.0f;
+float randomYBadCollectable = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnCollectables());
+        StartCoroutine(spawnBadCollectables());
+
+
+    }
+    IEnumerator spawnCollectables(){
+        while(true){
+            spawnCollectable();
+            yield return new WaitForSeconds(collectableSpawnTime);
+
+        }
+    }
+        IEnumerator spawnBadCollectables(){
+            while(true){
+                spawnBadCollectables();
+                yield return new WaitForSeconds(badCollectableSpawnTime);
+
+            }
+
+        }
+        void spawnCollectable(){
+            randomXCollectable = Random.Range(-maxX, maxX);
+            randomYCollectable = Random.Range(-maxY, maxY);
+            Instantiate(collectable, new Vector3(randomXCollectable, randomYCollectable, 0), Quaternion.identity);
+            
+        }
+        void spawnBadCollectable() {
+            randomXBadCollectable = Random.Range(-maxX, maxX);
+            randomYBadCollectable = Random.Range(-maxY, maxY);
+            Instantiate(badCollectable, new Vector3(randomXBadCollectable, randomYBadCollectable,0), Quaternion.identity);
+        
+   
+    }
+}
---
+++ First 2D/Assets/ScoreLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreLogic : MonoBehaviour
+{
+    public int score = 0;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = "Score: " + score;
+    }
+}
