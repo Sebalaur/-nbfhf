ee9dcfab2dad8778490f5f2a37f1bfd49a1c2dd8
Sebalaur <75026257+Sebalaur@users.noreply.github.com>
2020-12-20 12:50:02 +0200

    bradUnity

---
+++ Christmas Tree/Assets/camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    [SerializeField] private float zoomLerpSpeed = 10;
+
+    void Start()
+    {
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+        Debug.Log(scrollData);
+
+        targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, 4.5f, 8f);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+    }
+}
