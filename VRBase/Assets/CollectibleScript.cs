0f2d00bee6759f94b39a3637ec6f21cc238401ba
Sebalaur <75026257+Sebalaur@users.noreply.github.com>
2021-02-04 20:33:19 +0200

    lectieVR

---
+++ VRBase/Assets/CollectibleScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleScript : MonoBehaviour
+{
+    public GameObject collectible;
+    private bool focus = false;
+    void Start()
+    {
+        
+    }
+    public void LoadStart(){
+        focus = true;
+        StartCoroutine(Loading());
+    }
+    public void LoadStop()
+    {
+        focus = false;
+        StopAllCoroutines();
+    }
+    IEnumerator Loading(){
+        while(true){
+            yield return new WaitForSeconds(1f);
+            LoadingDone();
+        }
+    }
+    void LoadingDone(){
+        if(focus){
+            Destroy(collectible);
+            StopAllCoroutines();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
